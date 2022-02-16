a832f4ed817397b237bcd0a6b67846c0a0146d29
Stefan Layanto <stef.layanto@gmail.com>
2022-02-16 00:25:30 -0800

    adding raw counts

--- models.py
+++ models.py
@@ -13,7 +13,7 @@ Node Models (2/13/2022)
 """
 # Top level node class
 class Node:
-    def __init__(self, key, title, node_type, attrs=dict(), db_id=None):
+    def __init__(self, key, title, node_type, attrs=dict(), db_id=None, raw_count=1):
         assert type(key) == str, "Error: key must be a string"
         self.key = key
         self.title = title
@@ -22,6 +22,7 @@ class Node:
         self.set_attrs(attrs) # Attribute dictionary
         self.db_id = db_id # id from database. Populated when retrieving nodes
         self.parent_doc = None
+        self.raw_count = raw_count # stores the raw count that a node is repeated
         
     def __str__(self):
         return "{}:{}".format(self.title, self.type)
@@ -73,8 +74,9 @@ Edge-Related Models
 # Top level edge class
 # For source & dest, you can either pass in a Node object or tuple of (key, type)
 class Edge:
-    def __init__(self, label, source, dest, timestamp=0):
+    def __init__(self, label, source, dest, timestamp=0, raw_count=1):
         self.label = label
+        # Versatile constructors
         if type(source) == tuple:
             self.source_key, self.source_type = source
         else:
@@ -83,9 +85,11 @@ class Edge:
             self.dest_key, self.dest_type = dest
         else:
             self.dest_key, self.dest_type = dest.key, dest.type
+        
         self.attrs = dict() # Attribute dictionary
         self.time = datetime.date.fromtimestamp(timestamp)
         self.attrs['time'] = self.time
+        self.raw_count = raw_count # stores the raw count that an edge is repeated
         assert type(self.source_key) == str, "Error: source key must be a string"
         assert type(self.dest_key) == str, "Error: dest key must be a string"
         
